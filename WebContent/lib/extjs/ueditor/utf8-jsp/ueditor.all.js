4d5834afca63c014967408db6f70651de73711d7
huan <huan@huan-pc>
2018-07-23 14:33:32 +0800

    1.修改编辑ueditor最大10000个字符 2.服务器当前版本

--- WebContent/lib/extjs/ueditor/utf8-jsp/ueditor.all.js
+++ WebContent/lib/extjs/ueditor/utf8-jsp/ueditor.all.js
@@ -28624,7 +28624,7 @@ UE.ui = baidu.editor.ui = {};
             function setCount(editor,ui) {
                 editor.setOpt({
                     wordCount:true,
-                    maximumWords:10000,
+                    maximumWords:100000,
                     wordCountMsg:editor.options.wordCountMsg || editor.getLang("wordCountMsg"),
                     wordOverFlowMsg:editor.options.wordOverFlowMsg || editor.getLang("wordOverFlowMsg")
                 });
